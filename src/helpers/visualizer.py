19c28f5efb71138233ba568ae2d4ea5d4cbc4bb7
Lineax17 <lineax17@gmail.com>
2026-05-14 11:53:11 +0200

    update visualizer.py

--- src/helpers/visualizer.py
+++ src/helpers/visualizer.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 import cv2
+import numpy as np
 from matplotlib import pyplot as plt
 
 
@@ -10,28 +11,35 @@ class Visualizer:
         pass
 
     def show_bbox(self, image_path, annotation_path, title="Image"):
-        with open(annotation_path, "r") as f:
-            coco = json.load(f)
+        if isinstance(image_path, (str, Path)):
+            with open(annotation_path, "r") as f:
+                coco = json.load(f)
 
-        image_filename = Path(image_path).name
+            image_filename = Path(image_path).name
 
-        image_info = next(
-            img for img in coco["images"]
-            if img["file_name"] == image_filename
-        )
+            image_info = next(
+                img for img in coco["images"]
+                if img["file_name"] == image_filename
+            )
 
-        image_id = image_info["id"]
+            image_id = image_info["id"]
 
-        annotations = [
-            ann for ann in coco["annotations"]
-            if ann["image_id"] == image_id
-        ]
+            annotations = [
+                ann for ann in coco["annotations"]
+                if ann["image_id"] == image_id
+            ]
 
-        # Load image and bbox
-        image = cv2.imread(str(image_path))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # Load image and bbox
+            image = cv2.imread(str(image_path))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        bboxes = [ann["bbox"] for ann in annotations]
+            bboxes = [ann["bbox"] for ann in annotations]
+        else:
+            image = image_path
+            bboxes = annotation_path
+
+            if isinstance(image, np.ndarray) and image.ndim == 3 and image.shape[2] == 3:
+                image = image.astype(np.uint8, copy=False)
 
         image_vis = self.draw_bboxes(image, bboxes)
 
